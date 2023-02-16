9845deac41d2baf0b1a68a7dc514614d38061d8e
OyamaP <eag000gle.0.3151993@gmail.com>
2023-02-16 23:26:31 +0900

    add new event

--- src/modules/discord/Manager.ts
+++ src/modules/discord/Manager.ts
@@ -1,8 +1,9 @@
+import EventRegister from "./EventRegister.js";
 import { Client, GatewayIntentBits, Message } from "discord.js";
-import StampEvent from "./StampEvent.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 const { DISCORD_TOKEN } = process.env;
+const eventRegister = new EventRegister();
 
 /**
  * Discordとのログイン及びイベントを定義する
@@ -33,12 +34,11 @@ export default class DiscordManager {
   private messageCreateEvent = async (message: Message): Promise<void> => {
     try {
       if (message.author.bot) return;
-      if (StampEvent.isStampMessage(message))
-        await StampEvent.startStampBot(message);
+      eventRegister.startPrioritizedEvent(message);
       // console.log(message);
     } catch (e: any) {
       console.error(e);
-      message.channel.send(e.message || "Exception Error.");
+      // message.channel.send(e.message || "Exception Error.");
     }
   };
 }
